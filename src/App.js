df1aad817d3f081148a8e0e4e25c872273a7a506
AyDy Burling <aydy.you@gmail.com>
2020-02-11 18:27:22 -0600

    added about page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 //Don't change code unless discussed first with AyDy! Will disrupt navbar links! Thanks!
 //trying out links with local host 3000. Will merge with backend. Expect changes.
 import React from "react";
-import "bulma/css/bulma.css";
-import "react-bulma-components/src/index.sass";
+// import "bulma/css/bulma.css";
+// import "react-bulma-components/src/index.sass";
 
 import {
   BrowserRouter as Router,
@@ -13,6 +13,7 @@ import {
 
 import Users from "./user/pages/Users";
 import SearchGroups from "./groups/pages/SearchGroups";
+import AboutUs from "./groups/pages/AboutUs";
 import NewGroup from "./groups/pages/NewGroup";
 import UserGroups from "./groups/pages/UserGroups";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
@@ -34,6 +35,8 @@ const App = () => {
             <Route exact path="/" component={LoginAuth} />
 
             <PrivateRoute exact path="/search/" component={SearchGroups} />
+
+            <Route exact path="/about/" component={AboutUs} />
 
             <PrivateRoute exact path="/:userId/groups" component={UserGroups} />
 
