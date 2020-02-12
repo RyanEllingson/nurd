b4505d677e335969db13da03f4ae45178a8d8906
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2020-02-12 10:05:06 -0600

    fixed issue with search

--- src/groups/components/SearchGroupList.js
+++ src/groups/components/SearchGroupList.js
@@ -328,10 +328,10 @@ export default function SearchGroupList() {
             id: "outlined-age-native-simple"
           }}>
           <option value="" >Game Type</option>
-          <option value={"videoGames"}>Video Games</option>
-          <option value={"boardGames"}>Board Games</option>
-          <option value={"tradingCards"}>Trading Cards</option>
-          <option value={"other"}>Other</option>
+          <option value={"Video"}>Video Games</option>
+          <option value={"Board"}>Board Games</option>
+          <option value={"Card"}>Trading Cards</option>
+          <option value={"Other"}>Other</option>
 
         </Select>
         <button onClick={searchGroups}>Click here to search</button>
